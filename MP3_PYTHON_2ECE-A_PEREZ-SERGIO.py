5d65824f52b445ba3a4ec8bbef858e5e2cc024c2
Biroguuu <47405796+Biroguuu@users.noreply.github.com>
2019-12-09 00:10:27 +0800

    Add files via upload

--- MP3_PYTHON_2ECE-A_PEREZ-SERGIO.py
+++ MP3_PYTHON_2ECE-A_PEREZ-SERGIO.py
@@ -1,14 +1,15 @@
 import numpy as np
-def problem3x(p):
-    x = p[:,0]
-    y = p[:,1]
-    
+def MP3():
+    x = [int(x) for x in input("Enter multiple x value: ").split()] 
+    print("Number of list is: ", x)  
+    y = [int(y) for y in input("Enter multiple y value: ").split()] 
+    print("Number of list is: ", y)
+    #x = p[:,0]
+    #y = p[:,1]
     m = len(x)
     n = len(y)
-    
     e = []
     z = []
-    
     for v in range(2,12):
         if (n>=v) and (m>=v):
             d = np.polyfit(x,y,v-1)
